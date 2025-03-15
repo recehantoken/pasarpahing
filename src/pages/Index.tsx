58c521448a68a4e1ad266a1ed3187b18557d3526
Recehan Token <master@recehan.gold>
2025-03-16 05:13:34 +0700

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -140,7 +140,7 @@ const Index = () => {
           <BannerAd 
             title="New Arrivals"
             description="Check out our latest gold and silver collections fresh from the vault"
-            imageUrl="https://images.unsplash.com/photo-1610399214658-52b9fdea4627?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+            imageUrl="/kebaya.jpg"
             linkUrl="/new-arrivals"
             buttonText="Explore"
             className="w-full h-[250px]"
